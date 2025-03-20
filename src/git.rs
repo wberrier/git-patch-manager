0a65946c9536e21aed708ea2d46ed26c0c2a55a0
Wade Berrier <wberrier@gmail.com>
2025-03-20 17:01:25 -0600

    apply: use author date when applying patches

--- src/git.rs
+++ src/git.rs
@@ -40,7 +40,9 @@ pub fn apply_patches(
     patch_files: &[std::path::PathBuf],
 ) -> Result<()> {
     // Use 3way patch apply by default
-    let mut command = "git am --3way --whitespace=nowarn --ignore-whitespace".to_string();
+    let mut command =
+        "git am --committer-date-is-author-date --3way --whitespace=nowarn --ignore-whitespace"
+            .to_string();
 
     // Is it any faster to apply more than one patch at a time?
     for p in patch_files {
